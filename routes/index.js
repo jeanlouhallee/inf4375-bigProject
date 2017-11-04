2fb7a05fd660d6659df620b86e413c0abf0aa420
Jean-Lou <jeanlou601@hotmail.com>
2017-11-03 21:38:02 -0400

    Ajout de style a index.pug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Homepage'});
+    res.render('index', { title: 'Installations de la ville de Montréal'});
 });
 
 module.exports = router;
