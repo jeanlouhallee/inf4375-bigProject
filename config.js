68d2f91af1e0d106c9238088146b57884121a7a9
Jean-Lou <jeanlou601@hotmail.com>
2017-11-09 11:45:17 -0500

    Ajout des licenses.

--- config.js
+++ config.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Jean-Lou Hallée.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 //Fichier config
 
 exports.listOfAquaticInstallationsUrl = "http://donnees.ville.montreal.qc.ca/dataset/4604afb7-a7c4-4626-a3ca-e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/download/piscines.csv";
