d1242d46b82af2b3e1958765627a2fad7aab9fa2
Jean-Lou <jeanlou601@hotmail.com>
2017-11-26 22:12:15 -0500

    Correction du bug lors de la suppression

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -31,7 +31,7 @@ $(document).ready( function () {
         var id = $(this).attr('data-id');
 
         $.ajax({
-            url: '/installatins/' + id,
+            url: '/installations/' + id,
             type: 'DELETE',
             success: function(){
                 alert("data deleted!");
